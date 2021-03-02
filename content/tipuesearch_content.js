dadfa7c4f39b62f716b90d91fd4956bbe71b8708
40823219 <a0976682879@gmail.com>
2021-03-02 21:40:13 +0800

    更改標題

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '我的倉儲: https://github.com/40823219/cd2021 \n \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '我的倉儲: https://github.com/40823219/cd2021 \n 兩人小組:408232、40823231 \n \n', 'tags': '', 'url': 'About.html'}]};
