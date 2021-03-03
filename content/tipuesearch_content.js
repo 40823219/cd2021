fbdbcad455c9816196a92e4c7fcb8c8acce281b6
40823219 <a0976682879@gmail.com>
2021-03-03 22:57:51 +0800

    upload blog

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '課程網站: http://mde.tw/cad2020/content/index.html \n 我的倉儲: https://github.com/40823219/cd2021 \n 兩人小組:408232、40823231 \n \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '課程網站: http://mde.tw/cad2020/content/index.html \n 我的倉儲: https://github.com/40823219/cd2021 \n 兩人小組:40823219、40823231 \n \n', 'tags': '', 'url': 'About.html'}]};
